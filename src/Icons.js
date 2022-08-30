68a5ff0ad21b15d27618d821f5f7c4f1996884a7
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-08-30 18:42:30 +0300

    auth

--- src/Icons.js
+++ src/Icons.js
@@ -11,23 +11,88 @@ const SearchIcon = ({size}) => {
   return (
     <svg role="img" height="24" width="24" class="Svg-ytk21e-0 jAKAlG search-icon" viewBox="0 0 24 24">
       <path d="M10.533 1.279c-5.18 0-9.407 4.14-9.407 9.279s4.226 9.279 9.407 9.279c2.234 0 4.29-.77 5.907-2.058l4.353 4.353a1 1 0 101.414-1.414l-4.344-4.344a9.157 9.157 0 002.077-5.816c0-5.14-4.226-9.28-9.407-9.28zm-7.407 9.279c0-4.006 3.302-7.28 7.407-7.28s7.407 3.274 7.407 7.28-3.302 7.279-7.407 7.279-7.407-3.273-7.407-7.28z" fill="currentColor" />
-      </svg>
+    </svg>
   )
 }
 
 const CollectionIcon = ({size}) => {
   return (
-    <svg role="img" height="24" width="24" class="Svg-ytk21e-0 jAKAlG collection-icon" viewBox="0 0 24 24"><path d="M14.5 2.134a1 1 0 011 0l6 3.464a1 1 0 01.5.866V21a1 1 0 01-1 1h-6a1 1 0 01-1-1V3a1 1 0 01.5-.866zM16 4.732V20h4V7.041l-4-2.309zM3 22a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1zm6 0a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1z"  fill="currentColor" />
+    <svg role="img" height="24" width="24" class="Svg-ytk21e-0 jAKAlG collection-icon" viewBox="0 0 24 24">
+      <path d="M14.5 2.134a1 1 0 011 0l6 3.464a1 1 0 01.5.866V21a1 1 0 01-1 1h-6a1 1 0 01-1-1V3a1 1 0 01.5-.866zM16 4.732V20h4V7.041l-4-2.309zM3 22a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1zm6 0a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1z"  fill="currentColor" />
+    </svg>
+  )
+}
+
+const PlusIcon = ({size}) => {
+  return(
+    <svg role="img" height="12" width="12" aria-hidden="true" viewBox="0 0 16 16" class="Svg-ytk21e-0 jAKAlG">
+      <path d="M15.25 8a.75.75 0 01-.75.75H8.75v5.75a.75.75 0 01-1.5 0V8.75H1.5a.75.75 0 010-1.5h5.75V1.5a.75.75 0 011.5 0v5.75h5.75a.75.75 0 01.75.75z" fill="currentColor"  />
+    </svg>
+  )
+}
+
+const HeartIcon = ({ size }) => {
+  return (
+    <svg role="img" height="12" width="12" aria-hidden="true" viewBox="0 0 16 16" class="Svg-ytk21e-0 jAKAlG">
+      <path d="M15.724 4.22A4.313 4.313 0 0012.192.814a4.269 4.269 0 00-3.622 1.13.837.837 0 01-1.14 0 4.272 4.272 0 00-6.21 5.855l5.916 7.05a1.128 1.128 0 001.727 0l5.916-7.05a4.228 4.228 0 00.945-3.577z" fill="currentColor" />
+    </svg>
+  )
+}
+
+const BookMarkIcon = ({size}) => {
+  return (
+    <svg role="img" height="12" width="12" aria-hidden="true" viewBox="0 0 16 16" class="Svg-ytk21e-0 jAKAlG">
+      <path d="M3.75 0A1.75 1.75 0 002 1.75v12.952c0 1.051 1.22 1.633 2.037.972l3.962-3.208 3.943 3.204c.817.663 2.038.082 2.038-.97V1.75A1.75 1.75 0 0012.23 0H3.75z" fill="currentColor" />
+    </svg>
+  )
+}
+
+const DownloadIcon = ({size}) => {
+  return (
+    <svg role="img" height="24" width="24" viewBox="0 0 24 24" class="Svg-ytk21e-0 jAKAlG">
+      <path d="M12 3a9 9 0 100 18 9 9 0 000-18zM1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12z" fill="currentColor"/>
+      <path d="M12 6.05a1 1 0 011 1v7.486l1.793-1.793a1 1 0 111.414 1.414L12 18.364l-4.207-4.207a1 1 0 111.414-1.414L11 14.536V7.05a1 1 0 011-1z" fill="currentColor" />
+    </svg>
+  )
+}
+
+const PrevIcon = ({size}) => {
+  return (
+    <svg role="img" height="24" width="24" class="Svg-ytk21e-0 gFcOie IYDlXmBmmUKHveMzIPCF" viewBox="0 0 24 24">
+      <path d="M15.957 2.793a1 1 0 010 1.414L8.164 12l7.793 7.793a1 1 0 11-1.414 1.414L5.336 12l9.207-9.207a1 1 0 011.414 0z" fill="currentColor" />
+    </svg>
+  )
+}
+
+const NextIcon = ({size}) => {
+  return (
+    <svg role="img" height="24" width="24" class="Svg-ytk21e-0 gFcOie IYDlXmBmmUKHveMzIPCF" viewBox="0 0 24 24">
+      <path d="M8.043 2.793a1 1 0 000 1.414L15.836 12l-7.793 7.793a1 1 0 101.414 1.414L18.664 12 9.457 2.793a1 1 0 00-1.414 0z" fill="currentColor" />
+    </svg>
+  )
+}
+
+const DownDir = ({size}) => {
+  return (
+    <svg role="img" height="16" width="16" class="Svg-ytk21e-0 jAKAlG eAXFT6yvz37fvS1lmt6k" viewBox="0 0 16 16">
+      <path d="M14 6l-6 6-6-6h12z"  fill="currentColor"/>
     </svg>
   )
 }
 
 export const Icon = ({ name, size = 24 }) => {
   
-  const icons = {
+  const icons = { 
     home: HomeIcon,
     search: SearchIcon,
     collection: CollectionIcon,
+    plus: PlusIcon,
+    heart: HeartIcon,
+    bookmark: BookMarkIcon,
+    download: DownloadIcon,
+    prev: PrevIcon,
+    next: NextIcon,
+    downDir: DownDir,
   }
 
   const Components = icons[name]
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,8 @@
 import logo from '../img/logo.svg'
+import { Icon } from '../Icons'
 import Menu from "components/Sidebar/Menu"
+import Playlists from './Sidebar/Playlists'
+import DownloadApp from './Sidebar/DownloadApp'
 
 export default function Sidebar() {
   return (
@@ -9,6 +12,39 @@ export default function Sidebar() {
       </a>
 
       <Menu />
+
+      <nav className="mt-6">
+        <ul>
+          <li>
+            <a href="#" className='py-2 px-6 flex items-center group text-sm text-link font-semibold hover:text-white'>
+              <span className ="w-6 h-6 flex items-center justify-center mr-4 bg-white bg-opacity-60 group-hover:bg-opacity-100 rounded-sm text-black">
+                <Icon name="plus" size={12}/>
+              </span>
+              Create Playlist
+            </a>
+          </li>
+          <li>
+            <a href="#" className='py-2 px-6 flex items-center group text-sm text-link font-semibold hover:text-white'>
+              <span className ="w-6 h-6 flex items-center justify-center mr-4 bg-gradient-to-br from-purple-500 text-white rounded-sm to-blue-300 opacity-70 group-hover:bg-opacity-100">
+                <Icon name="heart" size={12}/>
+              </span>
+              Liked Songs
+            </a>
+          </li>
+          <li>
+            <a href="#" className='py-2 px-6 flex items-center group text-sm text-link font-semibold hover:text-white'>
+              <span className ="w-6 h-6 flex items-center justify-center mr-4 bg-green-700 bg-opacity-60 group-hover:bg-opacity-100 rounded-sm">
+                <Icon name="bookmark" size={12}/>
+              </span>
+              Your Episodes
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <Playlists />
+
+      <DownloadApp />
       
     </aside>
   )
