66054f2cf4f20f5ffdfa04fef973b2d5cb1fe8c2
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-02 08:01:03 +0300

    import component in app js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import "./App.css";
+import Sidebar from "./components/sidebar";
+import Navbar from "./components/navbar";
+import BottomBar from "./components/bottombar";
+import Content from "./components/content";
 
 function App() {
   return <div className=" bg-red-600">test</div>;
