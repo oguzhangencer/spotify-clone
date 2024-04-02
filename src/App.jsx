2f56ce7812cf9420c2e90a0bccd2887490db90d0
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-02 08:08:01 +0300

    components in app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import Sidebar from "./components/sidebar";
-import Navbar from "./components/navbar";
 import BottomBar from "./components/bottombar";
 import Content from "./components/content";
 
 function App() {
-  return <div className=" bg-red-600">test</div>;
+  return (
+    <>
+      <div>
+        <Sidebar />
+        <Content />
+      </div>
+      <BottomBar />
+    </>
+  );
 }
 
 export default App;
