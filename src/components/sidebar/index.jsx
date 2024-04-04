3e21d19b282e2e3b293abc37a5a11add348d77f0
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-04 08:07:34 +0300

    sidebar width

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,3 +1,9 @@
+import logo from "../../img/logo.svg";
+
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <div className=" w-60">
+      <img src={logo} alt="spotify" />
+    </div>
+  );
 }
