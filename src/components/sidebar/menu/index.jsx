b03393d65919a0b6b26b1300cc1a60208e6845d8
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-07 16:26:52 +0300

    menu component

---
+++ src/components/sidebar/menu/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Menu() {
+  return (
+    <nav className="px-2">
+      <ul>
+        <li>
+          <a href="#home">Anasayfa</a>
+        </li>
+        <li>
+          <a href="#search">Ara</a>
+        </li>
+        <li>
+          <a href="#">Kitaplığım</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
