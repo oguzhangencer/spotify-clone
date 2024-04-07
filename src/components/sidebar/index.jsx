aab06563320737679b501b4b476d636eb5746e72
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-07 16:27:14 +0300

    add menu component in sidebar component

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,9 +1,13 @@
 import logo from "../../img/logo.svg";
+import Menu from "./menu";
 
 export default function Sidebar() {
   return (
-    <div className=" w-60">
-      <img src={logo} alt="spotify" />
+    <div className="w-60 py-6 flex flex-col">
+      <a href="#" className="mb-5 px-6">
+        <img src={logo} alt="spotify" className="h-10" />
+      </a>
+      <Menu />
     </div>
   );
 }
