a9644d6c0ade0da0a3da519f76de49e15d466adb
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-10 13:34:11 +0300

    menus

--- src/components/sidebar/menu/index.jsx
+++ src/components/sidebar/menu/index.jsx
@@ -3,15 +3,30 @@ import React from "react";
 export default function Menu() {
   return (
     <nav className="px-2">
-      <ul>
+      <ul className="flex flex-col">
         <li>
-          <a href="#home">Anasayfa</a>
+          <a
+            href="#home"
+            className="h-10 flex items-center text-sm font-semibold text-white rounded hover:text-white px-4 bg-active"
+          >
+            Anasayfa
+          </a>
         </li>
         <li>
-          <a href="#search">Ara</a>
+          <a
+            href="#search"
+            className="h-10 flex items-center text-sm font-semibold text-link rounded hover:text-white px-4 "
+          >
+            Ara
+          </a>
         </li>
         <li>
-          <a href="#">Kitaplığım</a>
+          <a
+            href="#"
+            className="h-10 flex items-center text-sm font-semibold text-link rounded hover:text-white px-4 "
+          >
+            Kitaplığım
+          </a>
         </li>
       </ul>
     </nav>
