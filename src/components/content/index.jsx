fa6ef5c55077ca61e1dc93c883fe1ad523d5ad5e
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-11 11:40:30 +0300

    Refactor Content component to use react-router-dom in index.jsx

--- src/components/content/index.jsx
+++ src/components/content/index.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter as Routes, Route, Router } from "react-router-dom";
 import Navbar from "../navbar";
+import Home from "~/views/home";
+import Search from "~/views/search";
+import Collection from "~/views/collection";
 
 export default function Content() {
   return (
     <div className="flex-auto">
       <Navbar />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/collection" element={<Collection />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
