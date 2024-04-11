8aef26faaf086e6b3ff1662d898ce2ae393ad5ad
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-11 13:34:48 +0300

    Add Section component to Home view

---
+++ src/components/section/index.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom";
+
+export default function Section({ title, more = false }) {
+  return (
+    <section>
+      <header className="flex items-center justify-between mb-4">
+        <h3 className="text-2xl text-white font-semibold tracking-tight hover:underline">
+          {title}
+        </h3>
+        {more && (
+          <NavLink
+            to={more}
+            className="text-sm hover:underline font-semibold uppercase text-link tracking-wider"
+          >
+            See all
+          </NavLink>
+        )}
+      </header>
+    </section>
+  );
+}
