790df903d62c94c75ff16f3e5ab49f3560f6a0bd
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-11 11:40:24 +0300

    Refactor sidebar menu links to use NavLink component in index.jsx

--- src/components/sidebar/menu/index.jsx
+++ src/components/sidebar/menu/index.jsx
@@ -1,43 +1,50 @@
 import { Icon } from "~/Icons";
+import { NavLink } from "react-router-dom";
 
-export default function Menu() {
+function Menu() {
   return (
     <nav className="px-2">
       <ul className="flex flex-col">
         <li>
-          <a
-            href="home"
-            className="h-10 flex gap-x-4 items-center text-sm font-semibold text-white rounded hover:text-white px-4 bg-active"
+          <NavLink
+            activeClassName="bg-active text-white"
+            exact
+            to={"/"}
+            className="h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded hover:text-white px-4"
           >
             <span>
               <Icon name="home" />
             </span>
             Ana sayfa
-          </a>
+          </NavLink>
         </li>
         <li>
-          <a
-            href="search"
-            className="h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded hover:text-white px-4 "
+          <NavLink
+            activeClassName="bg-active text-white"
+            to={"/search"}
+            className="h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded hover:text-white px-4"
           >
             <span>
               <Icon name="search" />
             </span>
             Ara
-          </a>
+          </NavLink>
         </li>
         <li>
-          <a
-            href="collection"
-            className="h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded hover:text-white px-4 "
+          <NavLink
+            activeClassName="bg-active text-white"
+            to={"/collection"}
+            className="h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded hover:text-white px-4"
           >
             <span>
               <Icon name="collection" />
             </span>
-            Kitaplığım
-          </a>
+            Kitaplığın
+          </NavLink>
         </li>
       </ul>
     </nav>
   );
 }
+
+export default Menu;
