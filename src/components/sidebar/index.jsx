d86ef9c1ab06ee79c3c09db4d466a6ee477b9435
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-11 11:30:11 +0300

    Update sidebar logo link to point to home in index.jsx

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -4,7 +4,7 @@ import Menu from "./menu";
 export default function Sidebar() {
   return (
     <div className="w-60 py-6 flex flex-col">
-      <a href="#" className="mb-7 px-6">
+      <a href="home" className="mb-7 px-6">
         <img src={logo} alt="spotify" className="h-10" />
       </a>
       <Menu />
