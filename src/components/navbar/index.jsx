7f6d3ead6b674a951cb80df944a7d6ad7c58b21b
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-11 19:53:02 +0300

    Add search functionality to navbar component

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,10 +1,14 @@
 import Auth from "./auth";
 import Navigation from "./navigation";
+import Search from "./search";
+import { useSearchParams } from "react-router-dom";
 
 export default function Navbar() {
+  const searchRoute = useSearchParams("/search");
   return (
     <nav className="h-[3.75rem] flex items-center justify-between px-8 relative z-10">
       <Navigation />
+      {searchRoute && <Search />}
       <Auth />
     </nav>
   );
