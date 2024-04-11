0d23c9b5f96ebf7e492b1429f786aa2144253fac
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-11 12:26:04 +0300

    Add Auth component to Navbar

---
+++ src/components/navbar/auth/index.jsx
@@ -0,0 +1,7 @@
+export default function Auth() {
+  const user = {
+    name: "Oguzhan Gok",
+    avatar: "https://i.scdn.co/image/ab67757000003b820e33ce82e1d61cb128624dab",
+  };
+  return <div>Auth</div>;
+}
