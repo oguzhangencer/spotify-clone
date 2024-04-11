be32b72894637da7678eb43b09fa57e42e4d9585
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-11 13:34:29 +0300

    Refactor Sidebar component to include flex-shrink class in index.jsx

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -6,7 +6,7 @@ import DownloadApp from "./downloadApp";
 
 export default function Sidebar() {
   return (
-    <div className="w-60 py-6 flex flex-col">
+    <div className="w-60 py-6 flex flex-shrink flex-col">
       <a href="home" className="mb-7 px-6">
         <img src={logo} alt="spotify" className="h-10" />
       </a>
