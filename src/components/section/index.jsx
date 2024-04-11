0183f8a02ac31d4ed7688270e525a0ae0a40178e
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-11 19:53:16 +0300

    Refactor Section component to use Title component in index.jsx

--- src/components/section/index.jsx
+++ src/components/section/index.jsx
@@ -1,22 +1,10 @@
-import { NavLink } from "react-router-dom";
 import SongItem from "../songItem";
+import Title from "../title";
 
 export default function Section({ title, more = false, items = [] }) {
   return (
     <section>
-      <header className="flex items-center justify-between mb-4">
-        <h3 className="text-2xl text-white font-semibold tracking-tight hover:underline">
-          {title}
-        </h3>
-        {more && (
-          <NavLink
-            to={more}
-            className="text-sm hover:underline font-semibold uppercase text-link tracking-wider"
-          >
-            See all
-          </NavLink>
-        )}
-      </header>
+      <Title title={title} more={more} />
       <div className="grid grid-cols-5 gap-x-6">
         {items.map((item) => (
           <SongItem item={item} key={item.id} />
