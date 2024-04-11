594e786da6862e0cd5eff975b251d391ca629440
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-11 12:05:00 +0300

    Add Playlists and DownloadApp components to sidebar in index.jsx

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,5 +1,8 @@
+import { Icon } from "~/Icons";
 import logo from "../../img/logo.svg";
 import Menu from "./menu";
+import Playlists from "./playlists";
+import DownloadApp from "./downloadApp";
 
 export default function Sidebar() {
   return (
@@ -8,6 +11,35 @@ export default function Sidebar() {
         <img src={logo} alt="spotify" className="h-10" />
       </a>
       <Menu />
+      <nav className="mt-6">
+        <ul>
+          <li>
+            <a
+              href="/#"
+              className="py-2 px-6 flex items-center group text-sm text-link font-semibold hover:text-white "
+            >
+              <span className="w-6 h-6 flex items-center justify-center mr-4 bg-white bg-opacity-60 group-hover:bg-opacity-100 rounded-sm text-black">
+                <Icon name="plus" size={12} />
+              </span>
+              Çalma Listesi Oluştur
+            </a>
+          </li>
+          <li>
+            <a
+              href="/#"
+              className="py-2 px-6 flex items-center group text-sm text-link font-semibold hover:text-white "
+            >
+              <span className="w-6 h-6 flex items-center justify-center mr-4 bg-gradient-to-br from-purple-700 text-white rounded-sm to-blue-300 opacity-70 group-hover:opacity-100">
+                <Icon name="heart" size={12} />
+              </span>
+              Beğenilen Şarkılar
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <Playlists />
+      <DownloadApp />
     </div>
   );
 }
