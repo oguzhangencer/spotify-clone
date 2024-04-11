cfce9751c7d3fc56ec8bd2a4a6b058e697f71cd9
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-11 12:04:50 +0300

    Refactor App.jsx to use react-router-dom for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import Sidebar from "./components/sidebar";
 import BottomBar from "./components/bottombar";
 import Content from "./components/content";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <div className=" wrapper">
         <Sidebar />
         <Content />
       </div>
       <BottomBar />
-    </>
+    </Router>
   );
 }
 
