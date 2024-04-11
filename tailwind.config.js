b5befa9d336e1801dd135584730d1fc7571afd79
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-11 19:52:45 +0300

    Add tailwind.config.js with new font size and box shadow settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,12 @@ export default {
         link: "#b3b3b3",
         footer: "#181818",
       },
+      fontSize: {
+        s: "0.813rem",
+      },
+      boxShadow: {
+        spotify: "0 2px 4px 0 rgb(0 0 0 / 20%)",
+      },
     },
   },
   plugins: [],
