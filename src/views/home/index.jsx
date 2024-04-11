a30208d5838f3ff1e3f824d10eeec8112270bb07
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-11 13:41:48 +0300

    Update Section component in Home view to include items prop

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,9 +1,12 @@
 import Section from "~/components/section";
+import songs from "~/data/songs";
 
 export default function Home() {
   return (
-    <div>
-      <Section title="Recently played" more="/blabla" />
+    <div className="grid gap-y-8">
+      <Section title="Recently played" more="/blabla" items={songs} />
+      <Section title="Shows to try" more="/blabla" items={songs} />
+      <Section title="Made For Oguzhan Gok" more="/blabla" items={songs} />
     </div>
   );
 }
